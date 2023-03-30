d2394eda29c6172ece0a8e13efa566534595a9e1
Thais <scardoso.thais@gmail.com>
2023-03-30 17:22:58 -0300

    Atualizando deploy com anotação

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.generation.blogpessoal.configuration;
 
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
+@Configuration
 public class SwaggerConfig {
 
 @Bean	
